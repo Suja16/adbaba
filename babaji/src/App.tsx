3ccbf23e12af4ffd47204beec032488462fea823
Suja16 <sujal16@somaiya.edu>
2025-02-01 22:17:39 +0530

    home file made

--- babaji/src/App.tsx
+++ babaji/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Funnel from "./pages/funnel";
+import Home from "./pages/home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Funnel />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/funnel" element={<Funnel />} />
       </Routes>
     </Router>
   );
