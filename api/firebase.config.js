9da834a73c34b0fa617d846786e281ff6edf0dd3
VinayKanse <vinaykanse9451@gmail.com>
2025-02-01 22:40:53 +0530

    feat: add Firebase configuration and dependency to the project

---
+++ api/firebase.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyA0bksP3mhhp5idAgjUnSzwCImF1AGIl_c",
+  authDomain: "datathon2025-5b711.firebaseapp.com",
+  projectId: "datathon2025-5b711",
+  storageBucket: "datathon2025-5b711.firebasestorage.app",
+  messagingSenderId: "760497199890",
+  appId: "1:760497199890:web:730bc15adbd3082ffbb1cb",
+};
